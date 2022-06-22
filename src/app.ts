74e0d7fc6b7c6270a18bdd520b4828bb3d01583f
ashwin2200 <ashwincharath@gmail.com>
2022-06-22 11:48:01 -0700

    Server core checkpoint

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("Hello World");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import config from "config"; // Config file located in config/default.ts
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+const http = createServer(app);
+const io = new Server(http, {
+    cors: {
+        origin: corsOrigin,
+        credentials: true
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+http.listen(port, host, () => {
+    console.log(`Server listening on ${host}:${port}`);
+});
