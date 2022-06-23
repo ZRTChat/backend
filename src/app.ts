d8b15acd237cddc40a6432f5ce833966952a74e4
Alexander Ng <alex@ayo.icu>
2022-06-23 15:25:00 -0700

    it works

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,25 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import config from "config"; // Config file located in config/default.ts
+import { Socket } from "socket.io";
 import logger from "./utils/logger";
-import {version} from "../package.json";
-import socket from "./socket";
 
-const port = config.get<number>("port");
-const host = config.get<string>("host");
-const corsOrigin = config.get<string>("corsOrigin");
+const http = require('http').createServer();
 
-const app = express();
-const http = createServer(app);
-const io = new Server(http, {
-  cors: {
-    origin: corsOrigin,
-    credentials: true
-  }
+const io = require('socket.io')(http, {
+  cors: {origin: "*"}
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World! v' + version);
+io.on("connection", (socket: Socket) => {
+  logger.info(`User connected: ${socket.id}`);
+  socket.emit("message", "Hello World");
+  socket.on("message", (message: string) => {
+    logger.info(`User ${socket.id} sent message: ${message}`);
+    io.emit("message", message);
   }
+  );
+
+}
 );
 
-http.listen(port, host, () => {
-  logger.info(`Server ${version} listening on http://${host}:${port}/`);
-  socket({ io});
-});
+http.listen(4000, () => {
+  logger.info("Listening on port 4000");
+}
+);
