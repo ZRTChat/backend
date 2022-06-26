8cc3649d4b43a56de2cb9b2e07fb10c9203a5478
Alexander Ng <alex@ayo.icu>
2022-06-26 02:20:39 -0700

    prettier

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { Socket } from "socket.io";
 import logger from "./utils/logger";
 
-const http = require('http').createServer();
+const http = require("http").createServer();
 
-const io = require('socket.io')(http, {
-  cors: {origin: "*"}
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
 });
 
 io.on("connection", (socket: Socket) => {
@@ -12,13 +12,9 @@ io.on("connection", (socket: Socket) => {
   socket.on("message", (message: string) => {
     logger.info(`User ${socket.id} sent message: ${message}`);
     io.emit("message", message);
-  }
-  );
-
-}
-);
+  });
+});
 
 http.listen(4000, () => {
   logger.info("Listening on port 4000");
-}
-);
+});
